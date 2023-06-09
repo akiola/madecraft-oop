3bdc486dd3917f5ae892c61a424bb820e6913b11
GODFRED AWUKU <awuku@GODFREDs-MacBook-Pro.local>
2023-06-09 14:29:56 -0700

    Completed exercises for chapter 3

--- 3. Getters and Setters/exercises/Minion.java
+++ 3. Getters and Setters/exercises/Minion.java
@@ -2,4 +2,48 @@ package exercises;
 
 public class Minion {
 
+    private String name; 
+ 	private int eyes; 
+ 	private String color; 
+ 	private String master;
+
+    Minion(String name, int eyes, String color, String master){
+        this.name = name; 
+        this.eyes = eyes; 
+        this.color = color;
+        this.master = master;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setEyes(int newEyes){
+        this.eyes = newEyes;
+    }
+    public int getEyes(){
+        return this.eyes;
+    }
+
+    public void setColor(String newColor){
+        this.color = newColor;
+    }
+    public String getColor(){
+        return this.color;
+    }
+
+    public void setMaster(String newMaster){
+        this.master = newMaster;
+    }
+    public String getMaster(){
+        return this.master;
+    }
+
+
+
+
+
 }
